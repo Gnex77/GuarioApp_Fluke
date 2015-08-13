16a447d5e06e14900e3fd018045dc2807af42902
Guario Rodriguez <guario.rodriguez@gmail.com>
2015-08-12 23:12:48 -0400

    Initial CSS and HTML structure

--- src/components/GuarioApp.js
+++ src/components/GuarioApp.js
@@ -1,21 +1,38 @@
 'use strict';
 
 var React = require('react/addons');
-var ReactTransitionGroup = React.addons.TransitionGroup;
 
 // CSS
 require('normalize.css');
 require('../styles/main.css');
 
-var imageURL = require('../images/yeoman.png');
-
 var GuarioApp = React.createClass({
   render: function() {
     return (
-      <div className="main">
-        <ReactTransitionGroup transitionName="fade">
-          <img src={imageURL} />
-        </ReactTransitionGroup>
+      <div className="wrapper">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-3 col-md-2 sidebar">
+              <ul className="nav nav-sidebar">
+                <li className="active"><a href="#"><i className="fa fa-fw fa-lg fa-user"></i> Users <span className="sr-only">(current)</span></a></li>
+                <li><a href="#"><i className="fa fa-fw fa-lg fa-users"></i> Groups</a></li>
+              </ul>
+
+            </div>
+            <div className="col-sm-9 col-md-10 main">
+              <div className="list-view">
+                <ul className="list-unstyled">
+                  <li>Dylan, Bob</li>
+                  <li>Tyler, Steve</li>
+                  <li>Wilson, Ann</li>
+                </ul>
+              </div>
+              <div className="fab-create">
+                <button className="btn btn-success fab-btn" type="button" name="button"><i className="fa fa-plus fa-2x"></i></button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
